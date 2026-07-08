18db8fa7cee4228be60fd674a86c2e9d6f2bcf60
zuopengliang47-spec <zuopengliang47-spec@users.noreply.github.com>
2026-07-08 22:43:22 +0800

    恢复网站数据

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -1,5 +1,201 @@
 export const mockData = {
-  "categories": [],
+  "categories": [
+    {
+      "id": "my-favorites",
+      "name": "我的常用",
+      "icon": "💥",
+      "order": 0,
+      "sites": [
+        {
+          "id": "site-bilibili",
+          "name": "哔哩哔哩",
+          "url": "https://www.bilibili.com/",
+          "description": "弹幕视频网站",
+          "icon": "/sitelogo/www.bilibili.com.ico"
+        },
+        {
+          "id": "site-deepseek",
+          "name": "DeepSeek",
+          "url": "https://platform.deepseek.com/usage",
+          "description": "DeepSeek 开放平台",
+          "icon": "/sitelogo/platform.deepseek.com.ico"
+        },
+        {
+          "id": "site-douyu",
+          "name": "斗鱼直播",
+          "url": "https://www.douyu.com/directory/myFollow",
+          "description": "我的关注",
+          "icon": "/sitelogo/www.douyu.com.ico"
+        },
+        {
+          "id": "site-youtube",
+          "name": "YouTube",
+          "url": "https://www.youtube.com/",
+          "description": "视频分享平台",
+          "icon": "/sitelogo/www.youtube.com.ico"
+        },
+        {
+          "id": "site-github-trending",
+          "name": "GitHub Trending",
+          "url": "https://github.com/trending?since=monthly",
+          "description": "GitHub 月榜热门项目",
+          "icon": "/sitelogo/github.com.ico"
+        }
+      ]
+    },
+    {
+      "id": "entertainment",
+      "name": "影音娱乐",
+      "icon": "🎮",
+      "order": 2,
+      "sites": [
+        {
+          "id": "ent-bilibili",
+          "name": "哔哩哔哩",
+          "url": "https://www.bilibili.com/",
+          "description": "弹幕视频网站",
+          "icon": "/sitelogo/www.bilibili.com.ico"
+        },
+        {
+          "id": "ent-douyu",
+          "name": "斗鱼直播",
+          "url": "https://www.douyu.com/directory/myFollow",
+          "description": "游戏直播平台",
+          "icon": "/sitelogo/www.douyu.com.ico"
+        },
+        {
+          "id": "ent-qq-video",
+          "name": "腾讯视频",
+          "url": "https://v.qq.com/",
+          "description": "在线视频平台",
+          "icon": "/sitelogo/v.qq.com.ico"
+        },
+        {
+          "id": "ent-douyin",
+          "name": "抖音",
+          "url": "https://www.douyin.com/",
+          "description": "短视频平台",
+          "icon": "/sitelogo/www.douyin.com.ico"
+        },
+        {
+          "id": "ent-youtube",
+          "name": "YouTube",
+          "url": "https://www.youtube.com/",
+          "description": "全球视频平台",
+          "icon": "/sitelogo/www.youtube.com.ico"
+        },
+        {
+          "id": "ent-flingtrainer",
+          "name": "飞行训练器",
+          "url": "https://flingtrainer.com/",
+          "description": "PC游戏修改与模组",
+          "icon": "/sitelogo/flingtrainer.com.ico"
+        }
+      ]
+    },
+    {
+      "id": "software",
+      "name": "软件资源",
+      "icon": "💿",
+      "order": 4,
+      "sites": [
+        {
+          "id": "sw-hellowindows",
+          "name": "HelloWindows",
+          "url": "https://hellowindows.cn/",
+          "description": "精校完整Windows系统下载",
+          "icon": "/sitelogo/hellowindows.cn.ico"
+        },
+        {
+          "id": "sw-rightforum",
+          "name": "恩山无线论坛",
+          "url": "https://www.right.com.cn/forum/forum.php",
+          "description": "路由器与无线技术论坛",
+          "icon": "/sitelogo/www.right.com.cn.ico"
+        },
+        {
+          "id": "sw-pan",
+          "name": "智乃网盘",
+          "url": "https://pan.cinojiang.cc/",
+          "description": "资源网盘",
+          "icon": "/sitelogo/pan.cinojiang.cc.ico"
+        }
+      ]
+    },
+    {
+      "id": "community",
+      "name": "社区论坛",
+      "icon": "👥",
+      "order": 5,
+      "sites": [
+        {
+          "id": "com-yaohuo",
+          "name": "妖火网",
+          "url": "https://yaohuo.me/",
+          "description": "分享你我",
+          "icon": "/sitelogo/yaohuo.me.ico"
+        },
+        {
+          "id": "com-himiku",
+          "name": "初之音",
+          "url": "https://www.himiku.com/archives/homepage.html",
+          "description": "极简导航页",
+          "icon": "/sitelogo/www.himiku.com.ico"
+        },
+        {
+          "id": "com-linuxdo",
+          "name": "Linux.do",
+          "url": "https://linux.do",
+          "description": "Linux与开源技术社区",
+          "icon": "/sitelogo/linux.do.ico"
+        }
+      ]
+    },
+    {
+      "id": "network",
+      "name": "网络设备",
+      "icon": "📡",
+      "order": 6,
+      "sites": [
+        {
+          "id": "net-ikuai",
+          "name": "iKuai路由器",
+          "url": "http://192.168.9.1/",
+          "description": "iKuai软路由管理",
+          "icon": "/sitelogo/192.168.9.1.ico"
+        },
+        {
+          "id": "net-chinamobile",
+          "name": "中国移动光猫",
+          "url": "http://192.168.1.1/",
+          "description": "光猫管理页面",
+          "icon": "/sitelogo/192.168.1.1.ico"
+        }
+      ]
+    },
+    {
+      "id": "study",
+      "name": "学习考试",
+      "icon": "📚",
+      "order": 7,
+      "sites": [
+        {
+          "id": "stu-jxeea",
+          "name": "江西省教育考试院",
+          "url": "http://zkwb.jxeea.cn:8083/",
+          "description": "江西考试报名查分",
+          "icon": "/sitelogo/zkwb.jxeea.cn.ico"
+        },
+        {
+          "id": "stu-ustc-speed",
+          "name": "中科大测速",
+          "url": "https://test.ustc.edu.cn/",
+          "description": "中国科学技术大学测速网站",
+          "icon": "/sitelogo/test.ustc.edu.cn.ico"
+        }
+      ]
+    }
+  ],
   "title": "PengLiang导航",
   "search": "bing"
 }
